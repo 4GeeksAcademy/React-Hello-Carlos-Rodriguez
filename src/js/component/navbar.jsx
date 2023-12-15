7161f2c635703487a09da47e9be6aad59914de68
Carlos Rodriguez <139814946+CarlosRodriguez14@users.noreply.github.com>
2023-12-15 20:08:57 +0000

    last commit

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -4,11 +4,11 @@ import React from "react";
 const Navbar = () => {
     return (
             <div>
-                <nav className="navbar navbar-expand-lg bg-dark ">
+                <nav className="navbar navbar-expand-lg bg-dark  navbar-dark">
                     <div className="container-fluid">
                         <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
+                        <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon text-white"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                             <div className="navbar-nav justify-content-end flex-grow-1 pe-3">
